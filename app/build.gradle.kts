21b9bdb399de05a4337de3012a1dd6f33f446323
Natanael Ferreyra <natanaelferreyra2012@gmail.com>
2023-10-19 21:05:06 -0300

    versión 0.3 solucionando errores de ejecución

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.example.moviefan1"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.moviefan1"
-        minSdk = 24
-        targetSdk = 33
+        minSdk = 28
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
