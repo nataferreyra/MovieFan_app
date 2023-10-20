9ff2177e93c1a59ac32dc365f41c34e3865ccdc0
Tadeo <tadeomeyer@hotmail.com>
2023-10-19 22:29:19 -0300

    Version 0.4
    Agrego botones para ir a la pantalla catalogo, vista y destacados(este te manda a loggin ya que no encontre una pantalla al que mandarlo), tambien se agregan imaggeButton que te envian a la pantalla de VistaPeliculas

--- app/src/main/java/com/example/moviefan1/MainActivity.java
+++ app/src/main/java/com/example/moviefan1/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.moviefan1;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.SearchView;
 import android.os.Bundle;
 
@@ -10,6 +14,61 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button catalogobut=findViewById(R.id.botonCatalogo);
+        Button listabut=findViewById(R.id.botonListas);
+        Button destacadobut=findViewById(R.id.botonDestacados);
+        ImageButton peli1=findViewById(R.id.pelicula1);
+        ImageButton peli2=findViewById(R.id.pelicula2);
+        ImageButton peli3=findViewById(R.id.pelicula3);
+        ImageButton peli4=findViewById(R.id.pelicula4);
+        ImageButton peli5=findViewById(R.id.pelicula5);
+        ImageButton peli6=findViewById(R.id.pelicula6);
+
+        peli1.setOnClickListener(view -> {
+            Intent i=new Intent(MainActivity.this, vistaPeliculas.class);
+            startActivity(i);
+        });
+
+        peli2.setOnClickListener(view -> {
+            Intent i=new Intent(MainActivity.this, vistaPeliculas.class);
+            startActivity(i);
+        });
+
+        peli3.setOnClickListener(view -> {
+            Intent i=new Intent(MainActivity.this, vistaPeliculas.class);
+            startActivity(i);
+        });
+
+        peli4.setOnClickListener(view -> {
+            Intent i=new Intent(MainActivity.this, vistaPeliculas.class);
+            startActivity(i);
+        });
+
+        peli5.setOnClickListener(view -> {
+            Intent i=new Intent(MainActivity.this, vistaPeliculas.class);
+            startActivity(i);
+        });
+
+        peli6.setOnClickListener(view -> {
+            Intent i=new Intent(MainActivity.this, vistaPeliculas.class);
+            startActivity(i);
+        });
+
+        catalogobut.setOnClickListener(view -> {
+            Intent i=new Intent(MainActivity.this, catalogo.class);
+            startActivity(i);
+        });
+
+        listabut.setOnClickListener(view -> {
+            Intent i=new Intent(MainActivity.this, listas.class);
+            startActivity(i);
+        });
+
+        destacadobut.setOnClickListener(view -> {
+            Intent i=new Intent(MainActivity.this, loggin.class);
+            startActivity(i);
+        });
     }
     SearchView search_view;
 
